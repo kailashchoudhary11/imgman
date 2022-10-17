d4b241cbe91e0a46f5e7a185f259775cf9eef978
Kailash Choudhary <kailashjat604@gmail.com>
2022-10-17 19:21:50 +0530

    update static files dirs

--- imgman/settings.py
+++ imgman/settings.py
@@ -124,9 +124,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static"
+
 STATICFILES_DIRS = [
-    STATIC_ROOT,
+    BASE_DIR / "static",
 ]
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
