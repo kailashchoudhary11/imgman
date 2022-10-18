ad284a01ba4abbbc2786f49ed9f882d1bca3ccd4
kai <kailashjat604@gmail.com>
2022-10-18 06:57:17 +0530

    Moved CSS to static file

--- imgman/settings.py
+++ imgman/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
 
 MEDIA_URL = "/media/"
