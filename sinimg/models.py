c3cc0c42ddd17f5cb5929cd35abc737066a505a1
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-10-19 06:16:54 +0530

    message tags and some checks in views

--- sinimg/models.py
+++ sinimg/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class SinImg(models.Model):
     img = models.ImageField(upload_to="images/single")
+
+    def __str__(self):
+        return f"#{self.id} {self.img.name.split('/')[-1]}"
