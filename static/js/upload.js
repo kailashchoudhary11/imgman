1875ee1eabd48ceede94f0e101709ee85db96e6c
pdemeu <pdemeu@outlook.com>
2022-10-24 23:40:48 -0300

    Feat: extention file validation

--- static/js/upload.js
+++ static/js/upload.js
@@ -2,9 +2,13 @@ const inputImg = document.getElementById("id_img");
 
 inputImg.onchange = (e) => {
   const [file] = e.target.files
-  const fileName = file.name;
-  setFileNameValue(fileName);
-
+  if (fileValidation(file)) {
+    document.getElementById("upload_button").removeAttribute("disabled");
+    setFileNameValue(file.name);
+  } else {
+    alert("Please only image files");
+    document.getElementById("upload_button").setAttribute("disabled", "");
+  }
 }
 
 function allowDrop(ev) {
@@ -19,8 +23,12 @@ function drop(ev) {
   const [file] = ev.dataTransfer.files;
   const fileName = file.name;
   ev.preventDefault();
-  setFileNameValue(fileName);
-  setFile(file);
+    if (fileValidation(file)) {
+      setFileNameValue(fileName);
+      setFile(file);
+    }else{
+      alert("Please only image files")
+    }
 }
 
 function setFileNameValue(newName) {
@@ -30,9 +38,18 @@ function setFileNameValue(newName) {
 }
 
 function setFile(newFile) {
-  console.info(newFile);
   const hiddenInput = document.getElementById("id_img");
   const dataTransfer = new DataTransfer();
   dataTransfer.items.add(new File([newFile], newFile.name));
   hiddenInput.files = dataTransfer.files;
+}
+
+function fileValidation(file) {
+  // Regexp to validate extention file! Only Images formats
+  const validExtentionsRegex = /\.(jpe?g|png|gif|bmp)$/i;
+  if (validExtentionsRegex.test(file.name)){
+    return true
+  } else {
+    return false
+  }
 }
