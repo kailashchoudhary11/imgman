55dbf0b36d783a3dba7460e9fd2c8d950299a035
kai <kailashjat604@gmail.com>
2022-10-25 11:08:06 +0530

    Added contact view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 
 # define the home view
 class HomeView(TemplateView):
@@ -7,3 +7,10 @@ class HomeView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+class ContactView(View):
+    def get(self, request):
+        return render(request, "contact.html")
+    
+    def post(self, request):
+        pass
