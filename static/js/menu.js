6c0affd513d4dee5362c58051a5f959e96fb9199
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-10-24 22:51:25 -0500

    cleaned up comments

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,13 +1,6 @@
 $(document).ready(() => {
-    
-    // const navUl = document.querySelector('nav ul')
-    // const burgerNav = document.getElementsByClassName('burger-nav');
-    // burgerNav.addEventListener("click", function(evt) {
-    //     navUl.classList.toggle('open');
-    // })
 });
 
 $('.burger-nav').on('click', () => {
     $('nav ul').toggleClass("open");
-    // $('nav ul').hide();
 });
