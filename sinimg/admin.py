50447aca155a34ea2d190ec9317a8d27ab0457cc
Vaibhav Arora <vaibhavarora0301vishu@gmail.com>
2022-10-28 22:48:56 +0530

    Updated Admin Panel

--- sinimg/admin.py
+++ sinimg/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import SinImg
 
-admin.site.register(SinImg)
+
+@admin.register(SinImg)
+class SinImgAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image_path', 'created_at']
+    search_fields = ['img']
+    list_filter = ['created_at']
+
+    @admin.display(ordering='img')
+    def image_path(self, sinimg):
+        return sinimg.img
